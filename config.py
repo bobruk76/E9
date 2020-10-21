566971c74925ebfaad985c77469ef07bfe781d2b
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-10-21 16:16:39 +0300

    fix connections options

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     SECRET_KEY = os.urandom(32)
     PORT = os.getenv('PORT', 5000)
     SQLALCHEMY_DATABASE_URI = str(os.environ.get("DATABASE_URL",
-                                                 "postgresql://e9user:e9user@localhost:5432/e8"))
+                                                 "postgresql://e9user:e9user@localhost:5432/e9"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
