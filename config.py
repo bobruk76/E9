587b3a2cfc91ed2f132d0720e593f14f3d70e6ae
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-10-25 18:04:34 +0300

    add environment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.urandom(32)
+    SECRET_KEY = str(os.environ.get("SECRET_KEY",os.urandom(32)))
     PORT = os.getenv('PORT', 5000)
     SQLALCHEMY_DATABASE_URI = str(os.environ.get("DATABASE_URL",
                                                  "postgresql://e9user:e9user@localhost:5432/e9"))
